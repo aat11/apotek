f86f4d448714ce70fbf14e9ca21c2411f6f3def5
aat11 <msyuhadaaaa@gmail.com>
2023-01-16 03:28:32 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta http-equiv="refresh" content="1;url=http://localhost/Apotik-master/adminpages/index.php">
+  <title> Waiting </title>
+  <link rel="shortcut icon" href="assets/admin/images/favicon.ico">
+  <link rel="manifest" href="assets/admin/images/site.webmanifest">
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="main">
+    <div class="balls balls-1">
+		<div class="ball ball--1"></div>
+        <div class="ball ball--2"></div>
+        <div class="ball ball--3"></div>
+        <div class="ball ball--4"></div>
+    </div>
+    <div class="balls balls-2">
+		<div class="ball ball--1"></div>
+        <div class="ball ball--2"></div>
+        <div class="ball ball--3"></div>
+        <div class="ball ball--4"></div>
+    </div>
+</div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "sql203.epizy.com";
+$username = "epiz_33386583";
+$password = "ughx8jGtQff";
+$database_name = "epiz_33386583_apotek";
+
+$koneksi = mysqli_connect($host, $username, $password, $database_name) or die ("Koneksi Gagal");
+
+
+?>
